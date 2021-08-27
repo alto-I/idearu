8d92243be7134c35b54ee502e7379f4a4e1ce07b
alto-I <blmaldiy@gmail.com>
2021-08-27 17:26:11 +0900

    ideaindexAPIの実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'ideas#index'
+  namespace :api do
+    namespace :v1 do
+      resources :ideas, only: %i[index]
+    end
+  end
 end
