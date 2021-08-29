28c7c42f1d56eaaa9a9c800b53e1e430be0ccdc5
alto-I <blmaldiy@gmail.com>
2021-08-29 15:09:24 +0900

    rubocopとファイルの整理

--- app/javascript/reducers/ideas.js
+++ app/javascript/reducers/ideas.js
@@ -1,28 +1,28 @@
-import { REQUEST_STATE } from "../constants";
+import { REQUEST_STATE } from '../constants'
 
 export const initialState = {
   fetchState: REQUEST_STATE,
   ideasList: []
-};
+}
 
 export const ideasActionTypes = {
-  FETCHING: "FETCHING",
-  FETCH_SUCCESS: "FETCH_SUCCESS",
-};
+  FETCHING: 'FETCHING',
+  FETCH_SUCCESS: 'FETCH_SUCCESS'
+}
 
 export const ideasReducer = (state, action) => {
   switch (action.type) {
     case ideasActionTypes.FETCHING:
       return {
         ...state,
-        fetchState: REQUEST_STATE.LOADING,
-      };
+        fetchState: REQUEST_STATE.LOADING
+      }
     case ideasActionTypes.FETCH_SUCCESS:
       return {
         fetchState: REQUEST_STATE.OK,
-        ideasList: action.payload.ideas,
-      };
+        ideasList: action.payload.ideas
+      }
     default:
-      throw new Error();
+      throw new Error()
   }
-};
+}
