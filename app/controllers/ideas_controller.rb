800e57ba89bd4f051cf552a536a2e9f2e393be53
alto-I <blmaldiy@gmail.com>
2021-08-29 19:28:27 +0900

    idea個別ページの作成

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 class IdeasController < ApplicationController
+  before_action :set_idea, only: %i[show]
   def index
     @ideas = Idea.all
+  end
+
+  def show; end
+
+  private
+
+  def set_idea
+    @idea = Idea.find(params[:id])
   end
 end
