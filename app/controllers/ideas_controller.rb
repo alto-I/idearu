1faaac89ec4a027889a44332635a552844e02ec4
alto-I <blmaldiy@gmail.com>
2021-09-01 16:12:28 +0900

    投稿機能の実装

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,9 +2,12 @@
 
 class IdeasController < ApplicationController
   before_action :set_idea, only: %i[show]
+  before_action :authenticate_user!, only: %i[new confirm create]
   def index
     @ideas = Idea.all
   end
+
+  def show; end
 
   def new
     @idea = Idea.new
@@ -12,11 +15,24 @@ class IdeasController < ApplicationController
 
   def confirm
     title = "#{params[:problem]}という問題を解決したい"
-    elevatorpitch = create_elevorpitch(params[:service_name],params[:problem],params[:target],params[:category],params[:appeal_point],params[:competitive_services],params[:differentiation_factor])
+    elevatorpitch =
+      create_elevorpitch(
+        params[:service_name],
+        params[:problem],
+        params[:target],
+        params[:category],
+        params[:appeal_point],
+        params[:competitive_services],
+        params[:differentiation_factor],
+      )
     @idea = Idea.new(title: title, elevatorpitch: elevatorpitch)
   end
 
-  def show; end
+  def create
+    @idea = current_user.ideas.new(idea_params)
+    @idea.save
+    redirect_to idea_path(id: @idea.id)
+  end
 
   private
 
@@ -24,7 +40,19 @@ class IdeasController < ApplicationController
     @idea = Idea.find(params[:id])
   end
 
-  def create_elevorpitch(service_name,problem, target, category, appeal_point, competitive_services, differentiation_factor)
+  def create_elevorpitch(
+    service_name,
+    problem,
+    target,
+    category,
+    appeal_point,
+    competitive_services,
+    differentiation_factor
+  )
     "#{service_name}というサービスは、#{problem}という問題を解決したい#{target}向けの、#{category}です。ユーザーは#{appeal_point}ができ、#{competitive_services}とは違って、#{differentiation_factor}事が特徴です。"
+  end
+
+  def idea_params
+    params.require(:idea).permit(:title, :elevatorpitch)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'ideas#index'
   post 'ideas/new/confirm', to: 'ideas#confirm'
-  resources :ideas, only: %i[show new]
+  resources :ideas, only: %i[show new create]
   namespace :api do
     namespace :v1 do
       resources :ideas, only: %i[index show]
