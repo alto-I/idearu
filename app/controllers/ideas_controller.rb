4837084094fdd39f2d02ddf6c73e05365bee5f35
alto-I <blmaldiy@gmail.com>
2021-09-01 10:16:10 +0900

    新規投稿確認ページの作成

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -6,11 +6,23 @@ class IdeasController < ApplicationController
     @ideas = Idea.all
   end
 
+  def new
+    @idea = Idea.new
+  end
+
+  def confirm
+    @idea = Idea.new(idea_params)
+  end
+
   def show; end
 
   private
 
   def set_idea
     @idea = Idea.find(params[:id])
+  end
+
+  def idea_params
+    params.permit(:title, :elevatorpitch)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'ideas#index'
-  resources :ideas, only: %i[show]
+  post 'ideas/confirm', to: 'ideas#confirm'
+  resources :ideas, only: %i[show new]
   namespace :api do
     namespace :v1 do
       resources :ideas, only: %i[index show]
