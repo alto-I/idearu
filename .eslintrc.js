bb40d81e901f943364f0cf3a5bede21597c5eaf4
alto-I <blmaldiy@gmail.com>
2021-09-02 21:55:22 +0900

    関係パッケージの追加インストールと設定ファイルの作成

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:flowtype/recommended',
+    'prettier',
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['babel', 'flowtype', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'react/default-props-match-prop-types': 'off',
+    'react/require-default-props': 'off',
+    'flowtype/no-types-missing-file-annotation': 'off',
+    'flowtype/define-flow-type': 'warn',
+    'flowtype/use-flow-type': 'warn',
+  },
+}
