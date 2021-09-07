9db94bedaa01ff028915be6625d346b00aa50af1
alto-I <blmaldiy@gmail.com>
2021-09-07 18:17:05 +0900

    マイページのデザイン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: %i[show]
   before_action :set_user, only: %i[show]
 
   def show; end
