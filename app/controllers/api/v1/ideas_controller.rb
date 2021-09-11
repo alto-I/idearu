e7504b35e6a6c2ab9a09c0559a4de9f5cf1bfcf9
alto-I <blmaldiy@gmail.com>
2021-09-11 16:18:37 +0900

    fakeloadの削除

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -3,7 +3,6 @@
 module Api
   module V1
     class IdeasController < ApplicationController
-      before_action :fake_load
       before_action :set_idea, only: %i[show]
 
       def index
@@ -21,10 +20,6 @@ module Api
 
       def set_idea
         @idea = Idea.find(params[:id])
-      end
-
-      def fake_load
-        sleep(0.2)
       end
     end
   end
