05ba223dab925b79e53eef9ba9ddb73f128bc25a
alto-I <blmaldiy@gmail.com>
2021-09-11 16:14:15 +0900

    ideaコントローラーのファイル名の頭文字を小文字に変更

---
+++ app/controllers/api/v1/ideas_controller.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class IdeasController < ApplicationController
+      before_action :fake_load
+      before_action :set_idea, only: %i[show]
+
+      def index
+        @ideas =
+          if params[:solved] == 'false'
+            Idea.all.where(solved: false).order(created_at: 'DESC')
+          else
+            Idea.all.where(solved: true).order(created_at: 'DESC')
+          end
+      end
+
+      def show; end
+
+      private
+
+      def set_idea
+        @idea = Idea.find(params[:id])
+      end
+
+      def fake_load
+        sleep(0.2)
+      end
+    end
+  end
+end
