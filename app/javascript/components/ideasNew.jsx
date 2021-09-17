458a5a12b1488a57263657c922bb064250cbc595
alto-I <blmaldiy@gmail.com>
2021-09-17 18:05:53 +0900

    フォームに入力必須のバリデーションを設定

--- app/javascript/components/ideasNew.jsx
+++ app/javascript/components/ideasNew.jsx
@@ -1,11 +1,20 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { Fragment, useState } from 'react'
 import axios from 'axios'
+import { useForm } from 'react-hook-form'
+import '../stylesheets/ideasNew.css'
 
 axios.defaults.headers['X-CSRF-TOKEN'] = document
   .querySelector('meta[name="csrf-token"]')
   .getAttribute('content')
 
 const IdeasNew = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
   const [formValues, setFormValues] = useState({
     problem: '解決したい問題',
     target: 'ターゲット',
@@ -32,118 +41,141 @@ const IdeasNew = () => {
         <div className="form-container column">
           質問に対する回答を入力して下さい。
           <br />
-          <label htmlFor="problem">
-            1.解決したい問題は？
+          <form onSubmit={handleSubmit(postIdea)}>
+            <label htmlFor="problem">
+              <p>1.解決したい問題は？</p>
+              {errors.problem && (
+                <p className="error-message">{errors.problem.message}</p>
+              )}
+              <input
+                size="40"
+                type="text"
+                name="problem"
+                id="problem"
+                {...register('problem', {
+                  required: 'この入力項目は必須です。',
+                })}
+                onChange={(e) =>
+                  setFormValues({ ...formValues, problem: e.target.value })
+                }
+              />
+            </label>
             <br />
-            <input
-              size="40"
-              type="text"
-              name="problem"
-              id="problem"
-              onChange={(e) =>
-                setFormValues({ ...formValues, problem: e.target.value })
-              }
-            />
-          </label>
-          <br />
-          <label htmlFor="target">
-            2.このサービスを使うターゲットは？
+            <label htmlFor="target">
+              <p>2.このサービスを使うターゲットは？</p>
+              {errors.target && (
+                <p className="error-message">{errors.target.message}</p>
+              )}
+              <input
+                size="40"
+                type="text"
+                id="target"
+                {...register('target', {
+                  required: 'この入力項目は必須です。',
+                })}
+                onChange={(e) =>
+                  setFormValues({ ...formValues, target: e.target.value })
+                }
+              />
+            </label>
             <br />
-            <input
-              size="40"
-              type="text"
-              name="target"
-              id="target"
-              onChange={(e) =>
-                setFormValues({ ...formValues, target: e.target.value })
-              }
-            />
-          </label>
-          <br />
-          <label htmlFor="service">
-            3.サービス名は？（任意）
+            <label htmlFor="service">
+              <p>3.サービス名は？（任意）</p>
+              <input
+                size="30"
+                type="text"
+                id="service"
+                onChange={(e) =>
+                  setFormValues({ ...formValues, service: e.target.value })
+                }
+              />
+            </label>
             <br />
-            <input
-              size="30"
-              type="text"
-              name="service"
-              id="service"
-              onChange={(e) =>
-                setFormValues({ ...formValues, service: e.target.value })
-              }
-            />
-          </label>
-          <br />
-          <label htmlFor="category">
-            4.このサービスのカテゴリーは？
+            <label htmlFor="category">
+              <p>4.このサービスのカテゴリーは？</p>
+              {errors.category && (
+                <p className="error-message">{errors.category.message}</p>
+              )}
+              <input
+                size="40"
+                type="text"
+                id="category"
+                {...register('category', {
+                  required: 'この入力項目は必須です。',
+                })}
+                onChange={(e) =>
+                  setFormValues({ ...formValues, category: e.target.value })
+                }
+              />
+            </label>
             <br />
-            <input
-              size="40"
-              type="text"
-              name="category"
-              id="category"
-              onChange={(e) =>
-                setFormValues({ ...formValues, category: e.target.value })
-              }
-            />
-          </label>
-          <br />
-          <label htmlFor="appealPoint">
-            5.このサービスのアピールポイント、主要機能は？
+            <label htmlFor="appealPoint">
+              <p>5.このサービスのアピールポイント、主要機能は？</p>
+              {errors.appealPoint && (
+                <p className="error-message">{errors.appealPoint.message}</p>
+              )}
+              <input
+                size="60"
+                type="text"
+                id="appealPoint"
+                {...register('appealPoint', {
+                  required: 'この入力項目は必須です。',
+                })}
+                onChange={(e) =>
+                  setFormValues({ ...formValues, appealPoint: e.target.value })
+                }
+              />
+            </label>
             <br />
-            <input
-              size="60"
-              type="text"
-              name="appealPoint"
-              id="appealPoint"
-              onChange={(e) =>
-                setFormValues({ ...formValues, appealPoint: e.target.value })
-              }
-            />
-          </label>
-          <br />
-          <label htmlFor="competitiveServices">
-            6.競合サービスは？
+            <label htmlFor="competitiveServices">
+              <p>6.競合サービスは？</p>
+              {errors.competitiveServices && (
+                <p className="error-message">
+                  {errors.competitiveServices.message}
+                </p>
+              )}
+              <input
+                size="30"
+                type="text"
+                id="competitiveServices"
+                {...register('competitiveServices', {
+                  required: 'この入力項目は必須です。',
+                })}
+                onChange={(e) =>
+                  setFormValues({
+                    ...formValues,
+                    competitiveService: e.target.value,
+                  })
+                }
+              />
+            </label>
             <br />
-            <input
-              size="30"
-              type="text"
-              name="competitiveServices"
-              id="competitiveServices"
-              onChange={(e) =>
-                setFormValues({
-                  ...formValues,
-                  competitiveService: e.target.value,
-                })
-              }
-            />
-          </label>
-          <br />
-          <label htmlFor="differentiationFactor">
-            7.競合サービスとの差別化要素は？
+            <label htmlFor="differentiationFactor">
+              <p>7.競合サービスとの差別化要素は？</p>
+              {errors.differentiationFactor && (
+                <p className="error-message">
+                  {errors.differentiationFactor.message}
+                </p>
+              )}
+              <input
+                size="60"
+                type="text"
+                id="differentiationFactor"
+                {...register('differentiationFactor', {
+                  required: 'この入力項目は必須です。',
+                })}
+                onChange={(e) =>
+                  setFormValues({
+                    ...formValues,
+                    differentiationFactor: e.target.value,
+                  })
+                }
+              />
+            </label>
             <br />
-            <input
-              size="60"
-              type="text"
-              name="differentiationFactor"
-              id="differentiationFactor"
-              onChange={(e) =>
-                setFormValues({
-                  ...formValues,
-                  differentiationFactor: e.target.value,
-                })
-              }
-            />
-          </label>
-          <br />
-          <br />
-          <button
-            className="button is-success"
-            type="button"
-            onClick={postIdea}
-          >
-            投稿
-          </button>
+            <br />
+            <input type="submit" className="button is-success" />
+          </form>
         </div>
         <div className="image-container column">
           <div className="title">新規投稿イメージ</div>
