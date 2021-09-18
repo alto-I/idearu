4d633787573523c0bec3ec12230d47f77bf86cf5
alto-I <blmaldiy@gmail.com>
2021-09-18 19:06:53 +0900

    ソートボタンをドロップダウンに修正

--- app/javascript/components/ideas.jsx
+++ app/javascript/components/ideas.jsx
@@ -1,8 +1,11 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useEffect, useReducer, useState } from 'react'
 import '../stylesheets/ideas.css'
 import DayJS from 'react-dayjs'
 
-import { FaRegHeart, FaComment, FaPlus } from 'react-icons/fa'
+import { FaRegHeart, FaComment, FaPlus, FaAngleDown } from 'react-icons/fa'
 import fetchIdeas from '../apis/ideas'
 import { REQUEST_STATE } from '../constants'
 
@@ -12,6 +15,7 @@ const Ideas = () => {
   const [state, dispatch] = useReducer(ideasReducer, initialState)
   const [solved, setSolved] = useState(false)
   const [sort, setSort] = useState('created')
+  const [dropDownIsActive, setDropDownIsActive] = useState(false)
 
   useEffect(() => {
     dispatch({ type: ideasActionTypes.FETCHING })
@@ -24,6 +28,14 @@ const Ideas = () => {
       })
     )
   }, [])
+
+  const switchDropDownIsActive = () => {
+    if (dropDownIsActive) {
+      setDropDownIsActive(false)
+    } else {
+      setDropDownIsActive(true)
+    }
+  }
 
   const ideaSortCreated = () => {
     const createdIdeaList = state.ideasList.sort((a, b) => {
@@ -39,6 +51,7 @@ const Ideas = () => {
       },
     })
     setSort('created')
+    switchDropDownIsActive()
   }
 
   const ideaSortLikes = () => {
@@ -55,6 +68,7 @@ const Ideas = () => {
       },
     })
     setSort('likes')
+    switchDropDownIsActive()
   }
 
   const ideaSortLatestComments = () => {
@@ -71,6 +85,7 @@ const Ideas = () => {
       },
     })
     setSort('comments')
+    switchDropDownIsActive()
   }
 
   const fetchIdeaSolved = () => {
@@ -104,45 +119,76 @@ const Ideas = () => {
   return (
     <div className="ideas-container">
       <div className="button-container">
+        <div
+          className={
+            dropDownIsActive ? 'dropdown is-active m-1' : 'dropdown m-1'
+          }
+        >
+          <div className="dropdown-trigger">
+            <button
+              className="button is-small"
+              type="button"
+              aria-haspopup
+              aria-controls="dropdown-menu"
+              onClick={switchDropDownIsActive}
+            >
+              <span>ソート</span>
+              <span className="icon is-small">
+                <FaAngleDown />
+              </span>
+            </button>
+          </div>
+          <div className="dropdown-menu" id="dropdown-menu" role="menu">
+            <div className="dropdown-content">
+              <a
+                className={
+                  sort === 'created'
+                    ? 'dropdown-item is-active'
+                    : 'dropdown-item'
+                }
+                onClick={ideaSortCreated}
+              >
+                新着順
+              </a>
+              <a
+                className={
+                  sort === 'likes' ? 'dropdown-item is-active' : 'dropdown-item'
+                }
+                onClick={ideaSortLikes}
+              >
+                欲しい！順
+              </a>
+              <a
+                className={
+                  sort === 'comments'
+                    ? 'dropdown-item is-active'
+                    : 'dropdown-item'
+                }
+                onClick={ideaSortLatestComments}
+              >
+                最新コメント順
+              </a>
+            </div>
+          </div>
+        </div>
         <div className="buttons has-addons m-1">
           <button
             className={
-              sort === 'created' ? 'button is-success is-selected' : 'button'
+              solved
+                ? 'button is-small'
+                : 'button is-small is-success is-selected'
             }
-            type="button"
-            onClick={ideaSortCreated}
-          >
-            新着順
-          </button>
-          <button
-            className={
-              sort === 'likes' ? 'button is-success is-selected' : 'button'
-            }
-            type="button"
-            onClick={ideaSortLikes}
-          >
-            欲しい！順
-          </button>
-          <button
-            className={
-              sort === 'comments' ? 'button is-success is-selected' : 'button'
-            }
-            type="button"
-            onClick={ideaSortLatestComments}
-          >
-            最新コメント順
-          </button>
-        </div>
-        <div className="buttons has-addons">
-          <button
-            className={solved ? 'button' : 'button is-success is-selected'}
             type="button"
             onClick={fetchIdeaNotSolved}
           >
             未解決
           </button>
           <button
-            className={solved ? 'button is-danger is-selected' : 'button'}
+            className={
+              solved
+                ? 'button is-small is-danger is-selected'
+                : 'button is-small'
+            }
             type="button"
             onClick={fetchIdeaSolved}
           >
