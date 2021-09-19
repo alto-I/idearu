5c922f75df326f9ba9ce42e00052a000fd5edef5
alto-I <blmaldiy@gmail.com>
2021-09-20 00:14:14 +0900

    ヘッダーをReactに移し、レスポンシブに対応

---
+++ app/javascript/components/header.jsx
@@ -0,0 +1,71 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from 'react'
+import '../stylesheets/header.scss'
+
+const Header = (props) => {
+  const { isUserSignedIn, currentUser } = props
+  const [isAcitve, setIsActive] = useState(false)
+
+  const handleIsActiveChange = () => {
+    if (isAcitve) {
+      setIsActive(false)
+    } else {
+      setIsActive(true)
+    }
+  }
+
+  return (
+    <nav
+      className="navbar islight"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <a className="navbar-item" href="/">
+          <img src="/assets/logo_8.png" alt="title" />
+        </a>
+        <a
+          role="button"
+          className={isAcitve ? 'navbar-burger is-active' : 'navbar-burger'}
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+          tabIndex={0}
+          onClick={handleIsActiveChange}
+        >
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+        </a>
+      </div>
+      <div className={isAcitve ? 'navbar-menu is-active' : 'navbar-menu'}>
+        <div className="navbar-end is-size-7">
+          {isUserSignedIn ? (
+            <>
+              <a className="navbar-item" href="/ideas/new">
+                新規投稿
+              </a>
+              <a className="navbar-item" href={`/users/${currentUser.id}`}>
+                マイページ
+              </a>
+              <a
+                className="navbar-item"
+                href="/users/sign_out"
+                data-method="delete"
+              >
+                ログアウト
+              </a>
+            </>
+          ) : (
+            <a className="navbar-item" href="/users/sign_in">
+              ログイン
+            </a>
+          )}
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
