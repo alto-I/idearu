7f8018e2e9c571778547c9920b93323bd5066e49
alto-I <blmaldiy@gmail.com>
2021-09-25 18:12:10 +0900

    不要なメソッドを削除

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -3,7 +3,6 @@
 module Api
   module V1
     class IdeasController < ApplicationController
-      before_action :set_idea, only: %i[show]
       before_action :authenticate_user!, only: %i[create]
 
       def index
@@ -15,20 +14,12 @@ module Api
           end
       end
 
-      def show; end
-
       def create
         title = params[:title]
         elevatorpitch = params[:elevatorpitch]
         @idea = current_user.ideas.new(title: title, elevatorpitch: elevatorpitch)
         @idea.save!
         render json: { idea: @idea }
-      end
-
-      private
-
-      def set_idea
-        @idea = Idea.find(params[:id])
       end
     end
   end
