395c0e9bf29b604b5497fae22ff7508db120c714
alto-I <blmaldiy@gmail.com>
2021-09-28 17:07:23 +0900

    deviseの登録画面のレイアウトが崩れていたので修正

--- test/integration/api/v1/ideas_test.rb
+++ test/integration/api/v1/ideas_test.rb
@@ -14,4 +14,9 @@ class Api::IdeasTest < ActionDispatch::IntegrationTest
     assert_response :ok
     assert_equal 10, not_solved_ideas_json['ideas'].length
   end
+
+  test 'POST /api/v1/ideas' do
+    post api_v1_ideas_path(params: { title: 'title', elevatorpitch: 'elevatorpitch' })
+    assert_response :redirect
+  end
 end
