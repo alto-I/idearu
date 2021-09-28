1d67729ef81d09485fb1b918d0fee6e62ef47d04
alto-I <blmaldiy@gmail.com>
2021-09-28 14:57:04 +0900

    Likeモデルのモデルテスト

---
+++ test/models/like_test.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class LikeTest < ActiveSupport::TestCase
+  test 'validation of  uniqueness is effective?' do
+    user = users(:user1)
+    idea = ideas(:solved_idea1)
+    like = Like.new(user: user, idea: idea)
+    assert like.valid?
+
+    like.save
+    another_like = Like.new(user: user, idea: idea)
+    assert_not another_like.valid?
+  end
+end
