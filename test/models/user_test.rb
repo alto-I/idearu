9f118478876dff1ad81d134f4dec015057da187f
alto-I <blmaldiy@gmail.com>
2021-09-28 13:16:06 +0900

    Userモデルのモデルテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,8 +3,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test 'name' do
-    # p ideas(:idea10)
-    assert_equal 'alto', users(:alto).name
+  test 'validation of name and email and password is effective?' do
+    no_name_user = User.new(name: '', email: 'sample@sample.com', password: 'password')
+    assert_not no_name_user.valid?
+
+    no_email_user = User.new(name: 'sample', email: '', password: 'password')
+    assert_not no_email_user.valid?
+
+    no_password_user = User.new(name: 'sample', email: 'sample@sample.com', password: '')
+    assert_not no_password_user.valid?
+
+    user = User.new(name: 'sample', email: 'sample@sample.com', password: 'password')
+    assert user.valid?
+
+    too_long_email_user = User.new(name: 'sample', email: 'samplesamplesamplesamplesamplesamplesample@sample.com', password: 'password')
+    assert_not too_long_email_user.valid?
+  end
+
+  test 'admin?' do
+    assert users(:admin).admin?
   end
 end
